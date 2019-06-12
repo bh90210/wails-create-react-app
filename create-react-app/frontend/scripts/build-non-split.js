76752bddcabac4a7949ecf9a5ead254e0e39cae6
ktc <ktc@protonmail.com>
2019-06-12 14:54:13 +0300

    custom react-scripts

--- create-react-app/frontend/scripts/build-non-split.js
+++ create-react-app/frontend/scripts/build-non-split.js
@@ -8,10 +8,24 @@ config.optimization.splitChunks = {
     default: false,
   },
 };
+
+//
+config.module.rules = {[
+  test: /\.(png|jpg|gif)$/i,
+  use: [
+  {
+    loader: 'url-loader',
+    options: {
+      limit: 9999999999999999,
+    },
+  },
+]};
+
 // Move runtime into bundle instead of separate file
 config.optimization.runtimeChunk = false;
 
 // JS
 config.output.filename = 'static/js/main.js';
+
 // CSS. "5" is MiniCssPlugin
 config.plugins[5].options.filename = 'static/css/main.css';
